79f88383fbc50a493ff5fafde1ae89d3bd873d86
jacinta <jacintawmwangi21@gmail.com>
2024-01-25 23:20:11 +0300

    added logic

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,18 +2,32 @@ class LinksController < ApplicationController
 
     def create
         @long_url = params['long_url']
-        @short_url = generate_short_url
         render json: {
             :message => "short_url created successfully",
-            :short_url => @short_url
+            :short_url => generate_short_url
             }, status: :ok
+    end
+
+    def redirect
+        
     end
 
     private
     def generate_short_url
-        short_code = Base64.urlsafe_encode64(@long_url)[0..6]
+        local_long_url = Link.find_by(long_url: @long_url)
+        if local_long_url
+            return local_long_url.short_url
+        end
+
+        short_code = SecureRandom.hex(4)
         base_url = "http://127.0.0.1:3000/"
-        base_url + short_code
+        @short_url = base_url + short_code
+        my_new_url=Link.new(
+            long_url: @long_url,
+            short_url: @short_url)
+        my_new_url.save
+        
+        my_new_url.short_url
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  
   post '/create', to: 'links#create'
+  get '/', to: 'links#redirect'
 
 end
