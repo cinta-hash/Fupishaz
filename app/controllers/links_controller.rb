23d28c91e1c45f00921eae2f3e59369315f29170
jacinta <jacintawmwangi21@gmail.com>
2024-01-25 17:09:42 +0300

    changes

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,13 +2,12 @@ class LinksController < ApplicationController
 
     def create
         @link = Link.new(link_params)
-        @link.short_url = link.custom_url.present? ? @link.custom_url: generate_short_url
         if @link.save
-            render :show
-        else 
-            render json: @link.errors, status: :unprocessable_entity
+          render json: {short_url: @link.short_url}, status: :created
+        else
+          render json: {errors: @link.errors}, status: :unprocessable_entity
         end
-    end
+      end
 
     def show
         @link = Link.find(params[:id])
@@ -18,21 +17,21 @@ class LinksController < ApplicationController
     def redirect 
         @link = Link.find_by(short_url: params[:short_url])
         if @link.nil?
-            render json: {error: "No link found with the provided Short URL"}, status: :not_found
+          render json: {error: "No link found with the provided Short URL"}, status: :not_found
         else
-            @link.update_attribute(:clicks, @link.clicks + 1)
-            redirect_to @link.long_url, :custom_url
+          @link.update_attribute(:clicks, @link.clicks + 1)
+          redirect_to @link.long_url, status: :found
         end
-    end
+      end
 
     private
 
     def link_params
-        params.require(:link).permit(:long_url, custom_url)
+        params.require(:link).permit(:long_url, :custom_url)
     end
 
     def generate_short_url
-        SecureRandom.urlsafe_base64(5)
+        Base64.urlsafe_encode64(@link.long_url)[0..6]
     end
 
 end
