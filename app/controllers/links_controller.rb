32af37a2f1acf578b3718fdc90e6f25af42eff6c
jacinta <jacintawmwangi21@gmail.com>
2024-01-26 12:38:14 +0300

    added clicks and custom logic

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,52 +1,70 @@
 class LinksController < ApplicationController
-
+    before_action :load_link, only: [:redirect]
+  
     def create
-        @long_url = params['long_url']
-        render json: {
-            :message => "short_url created successfully",
-            :short_url => generate_short_url
-            }, status: :ok
-    end
-
-    def redirect
-        @short_code = params["short_code"]
-        long_url = get_url_by_short_code
-
-        if long_url
-            redirect_to long_url, allow_other_host: true
+      @long_url = params['long_url']
+      @custom_url = params['custom_url']
+  
+      if @custom_url.present?
+        @short_url = custom_url
+        unless Link.find_by(custom_url: @custom_url)
+          create_link_record
         else
-            render json: {:long_url => long_url}, status: :not_found
+          render json: { :error => "Custom URL is already taken" }, status: :bad_request
+          return
         end
+      else
+        @short_url = generate_short_url
+      end
+  
+      render json: {
+        :message => "Short URL created successfully",
+        :short_url => @short_url
+      }, status: :ok
     end
-
+  
+    def redirect
+      @link.increment!(:clicks) # Increment the clicks count
+      redirect_to @link.long_url, allow_other_host: true
+    end
+  
     private
+  
     def generate_short_url
-        local_long_url = Link.find_by(long_url: @long_url)
-        if local_long_url
-            return local_long_url.short_url
-        end
-
-        short_code = SecureRandom.hex(4)
-        base_url = "http://127.0.0.1:3000/"
-        @short_url = base_url + short_code
-        my_new_url=Link.new(
-            long_url: @long_url,
-            short_url: @short_url)
-        my_new_url.save
-        
-        my_new_url.short_url
+      local_long_url = Link.find_by(long_url: @long_url)
+      return local_long_url.short_url if local_long_url
+  
+      short_code = SecureRandom.hex(4)
+      base_url = "http://127.0.0.1:3000/"
+      @short_url = base_url + short_code
+      create_link_record
+  
+      @short_url
     end
+  
     def get_url_by_short_code
-        base_url = "http://127.0.0.1:3000/"
-        @short_url = base_url + @short_code
-
-        link_record = Link.find_by(short_url: @short_url)
-        if link_record
-            return link_record.long_url
-        else 
-            return nil
-        end
+      link_record = Link.find_by(short_url: "http://127.0.0.1:3000/#{@short_code}")
+      link_record&.long_url
     end
-
-end
-
+  
+    def custom_url
+      "http://127.0.0.1:3000/#{@custom_url}"
+    end
+  
+    def create_link_record
+      my_new_url = Link.new(
+        long_url: @long_url,
+        short_url: @short_url,
+        custom_url: @custom_url
+      )
+      my_new_url.save
+    end
+  
+    def load_link
+      @short_code = params["short_code"]
+      @link = Link.find_by(short_url: "http://127.0.0.1:3000/#{@short_code}")
+  
+      render json: { :error => "Short URL not found" }, status: :not_found unless @link
+    end
+  end
+  
